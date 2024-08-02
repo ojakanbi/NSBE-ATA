0e7f7e46c7c7d59f1d8fc9521cdb94d6604ccbe0
Oyindamola Joshua Akanbi <ojakanbi@Oyindamolas-MacBook-Pro.local>
2024-08-02 11:55:37 -0700

    frontend modules modify

---
+++ nsbe-ata-frontend/src/app/components/Register/register.js
@@ -0,0 +1,9 @@
+import styles from "./register.css";
+
+export default function Register() {
+    return(
+        <h2 className={styles.h2}>
+            Register component
+        </h2>
+    )
+}
